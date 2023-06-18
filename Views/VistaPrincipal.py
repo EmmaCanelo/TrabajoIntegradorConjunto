e108c69bc84e92c77a291803753840b6f7da61b9
josececchel <116840877+josececchel@users.noreply.github.com>
2023-06-18 20:51:16 -0300

    Create Vista Evento, Modelo y Archivo, Vista Princ

--- Views/VistaPrincipal.py
+++ Views/VistaPrincipal.py
@@ -1,1 +1,35 @@
- 
+class VistaPrincipal:
+    def menu_presentacion(self):
+        print("\nBienvenido a SocialEvent S.A.")
+        print("Somos expertos en la organización de Congresos y Eventos.")
+        print("¡Haremos de tu evento una experiencia inolvidable!")
+        
+    def menu_opciones(self):
+        print("\nA continuación le brindamos el menu de opciónes:")
+        print("1. Ver lista de servicios disponibles")
+        print("2. Seleccionar servicios")
+        print("3. Modificar servicios")
+        print("4. Seleccionar fecha del evento")
+        print("5. Ver costo total")
+        print("6. Reservar evento")
+        print("7. Cancelar reserva")
+        print("0. Salir")
+        
+        
+    def saludo_final(self):
+        print("\n¡Gracias por utilizar nuestro programa de SocialEvent S.A.!")
+        print("Esperamos haber cumplido con tus expectativas.")
+        print("¡Te deseamos un evento exitoso y lleno de alegría!")
+        print("¡Hasta la próxima!\n")
+        
+        
+    def ingreso_menu(self):
+        return input("\nPor favor, selecciona una opción: ")  
+        
+
+    def error_ingreso(self):
+        print("\n***Error de ingreso, vuelva a intentarlo***")
+        
+        
+    def vista_atras(self):
+        return input("\nPresione 1 para volver al menu principal: ")
